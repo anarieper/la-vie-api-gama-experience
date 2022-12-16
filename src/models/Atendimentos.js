6468f38294236b550cdcfe109734c05874b5b2cc
Ana Caroline Rieper Pereira <98720019+anarieper@users.noreply.github.com>
2022-12-16 16:43:56 -0300

    Add files via upload
    Adding Atendimentos to models

---
+++ src/models/Atendimentos.js
@@ -0,0 +1,42 @@
+const db = require("../database");
+const { DataTypes } = require('sequelize');
+const { Psicologos } = require("./Psicologos");
+const { Pacientes } = require("./Pacientes");
+
+const Atendimentos = db.define("Atendimentos", {
+    atendimentos_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true, 
+    },
+    data_atendimento: {
+        type: DataTypes.DATE,
+    },
+    observacao: {
+        type: DataTypes.STRING,
+    }, 
+    createdAt: {
+        type: DataTypes.DATE,
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+    },
+    psicologos_psicologos_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Psicologos,
+            key: "psicologos_id",
+        }
+    },
+    pacientes_paciente_id:{
+        type: DataTypes.INTEGER, 
+        references: {
+            model: Pacientes, 
+            key: "paciente_id",
+        }
+    },
+}, {
+   tableName: "atendimentos", 
+});
+
+module.exports = Atendimentos;
